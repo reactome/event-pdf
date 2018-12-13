63c03f7b2436e9c4b277bca03ea04776f1ac0b51
Pascual Lorente <pasculorente@gmail.com>
2018-12-13 16:31:12 +0000

    Added authors to cover page

--- src/main/java/org/reactome/server/tools/document/exporter/section/CoverPage.java
+++ src/main/java/org/reactome/server/tools/document/exporter/section/CoverPage.java
@@ -5,6 +5,9 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
 import org.reactome.server.graph.domain.model.Event;
+import org.reactome.server.graph.domain.model.InstanceEdit;
+import org.reactome.server.graph.domain.model.Pathway;
+import org.reactome.server.graph.domain.model.Person;
 import org.reactome.server.tools.document.exporter.DocumentContent;
 import org.reactome.server.tools.document.exporter.style.Images;
 import org.reactome.server.tools.document.exporter.style.PdfProfile;
@@ -12,6 +15,11 @@ import org.reactome.server.tools.document.exporter.util.HtmlParser;
 import org.reactome.server.tools.document.exporter.util.PdfUtils;
 
 import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Chuan-Deng dengchuanbio@gmail.com
@@ -28,11 +36,14 @@ public class CoverPage implements Section {
 		document.add(profile.getTitle(""));
 		document.add(profile.getTitle(event.getDisplayName()));
 		document.add(profile.getTitle(""));
+		final List<String> authors = collectAuthors(event).stream().map(this::getName).collect(Collectors.toList());
+		final String auth = String.join(", ", authors);
+		document.add(profile.getParagraph(auth).setTextAlignment(TextAlignment.CENTER));
+
 		for (Paragraph paragraph : HtmlParser.parseText(profile, PdfUtils.getProperty("cover.page.disclaimer"))) {
 			document.add(paragraph.setTextAlignment(TextAlignment.CENTER));
 		}
-
-//		final String link = analysisData.getServerName() + "/PathwayBrowser/#/ANALYSIS=" + analysisData.getResult().getSummary().getToken();
+		//		final String link = analysisData.getServerName() + "/PathwayBrowser/#/ANALYSIS=" + analysisData.getResult().getSummary().getToken();
 
 //		final String text = PdfUtils.getProperty("cover.page",
 //				analysisData.getName(),
@@ -43,6 +54,25 @@ public class CoverPage implements Section {
 //		final Collection<Paragraph> paragraphs = HtmlParser.parseText(profile, text);
 //		for (Paragraph paragraph : paragraphs)
 //			document.add(paragraph.setTextAlignment(TextAlignment.CENTER));
+	}
+
+	private String getName(Person person) {
+		return person.getDisplayName() + ".";
+	}
+
+	private Collection<Person> collectAuthors(Event event) {
+		final Set<Person> authors = new HashSet<>();
+		for (InstanceEdit instanceEdit : event.getAuthored()) authors.addAll(instanceEdit.getAuthor());
+		for (InstanceEdit instanceEdit : event.getEdited()) authors.addAll(instanceEdit.getAuthor());
+		for (InstanceEdit instanceEdit : event.getReviewed()) authors.addAll(instanceEdit.getAuthor());
+		for (InstanceEdit instanceEdit : event.getRevised()) authors.addAll(instanceEdit.getAuthor());
+		if (event.getCreated() != null) authors.addAll(event.getCreated().getAuthor());
+		if (event.getModified() != null) authors.addAll(event.getModified().getAuthor());
+		if (event instanceof Pathway) {
+			final Pathway pathway = (Pathway) event;
+			for (Event hasEvent : pathway.getHasEvent()) authors.addAll(collectAuthors(hasEvent));
+		}
+		return authors;
 	}
 
 
