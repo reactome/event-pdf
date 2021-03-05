6adaa6035f4097c34de6591fb845b5f380e36367
EliotRagueneau <orkeliot@gmail.com>
2021-03-05 17:04:43 +0000

    Fix pdf exports on GSA analysis

--- src/main/java/org/reactome/server/tools/event/exporter/section/PropertiesSection.java
+++ src/main/java/org/reactome/server/tools/event/exporter/section/PropertiesSection.java
@@ -3,6 +3,7 @@ package org.reactome.server.tools.event.exporter.section;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
 import org.reactome.server.analysis.core.result.model.AnalysisSummary;
 import org.reactome.server.tools.event.exporter.AnalysisData;
 import org.reactome.server.tools.event.exporter.DocumentContent;
@@ -21,44 +22,58 @@ import java.util.List;
  */
 public class PropertiesSection implements Section {
 
-	private final static String PROJECTED = "/documentation/inferred-events";
-	private final static String ANALYSIS_PATH = "/user/guide/analysis";
+    private final static String PROJECTED = "/documentation/inferred-events";
+    private final static String ANALYSIS_PATH = "/user/guide/analysis";
+    private final static String GSA_ANALYSIS_PATH = "/user/guide/analysis/gsa";
 
-	@Override
-	public void render(Document document, DocumentContent content) {
-		if (content.getAnalysisData() == null) return;
-		final PdfProfile profile = content.getPdfProfile();
-		final AnalysisData analysisData = content.getAnalysisData();
-		document.add(new AreaBreak());
-		document.add(profile.getH1("Analysis properties").setDestination("properties"));
-		final List<Paragraph> list = new LinkedList<>();
+    @Override
+    public void render(Document document, DocumentContent content) {
+        if (content.getAnalysisData() == null) return;
+        final PdfProfile profile = content.getPdfProfile();
+        final AnalysisData analysisData = content.getAnalysisData();
+        document.add(new AreaBreak());
+        document.add(profile.getH1("Analysis properties").setDestination("properties"));
+        final List<Paragraph> paragraphs = new LinkedList<>();
 
-		final String text = Texts.getProperty(analysisData.getResult().getSummary().getType().toLowerCase());
-		final int found = analysisData.getResult().getAnalysisIdentifiers().size();
-		final int notFound = analysisData.getResult().getNotFound().size();
-		final AnalysisSummary summary = analysisData.getResult().getSummary();
+        final AnalysisSummary summary = analysisData.getResult().getSummary();
+        final int found = analysisData.getResult().getAnalysisIdentifiers().size();
+        final int notFound = analysisData.getResult().getNotFound().size();
+        final String serverName = analysisData.getServerName();
+        final String analysisTypeDescription;
+        final String gsaMethod = summary.getGsaMethod();
+        final Text analysisTypeSeeMoreLink;
 
+        if (gsaMethod == null) {
+            analysisTypeSeeMoreLink = profile.getLink("See more", serverName + ANALYSIS_PATH);
+            String analysisType = summary.getType().toLowerCase();
+            analysisTypeDescription = Texts.getProperty(analysisType);
+        } else {
+            analysisTypeSeeMoreLink = profile.getLink("See more", serverName + GSA_ANALYSIS_PATH);
+            analysisTypeDescription = Texts.getProperty(gsaMethod.toLowerCase());
+        }
 
-		final String serverName = analysisData.getServerName();
-		list.add(HtmlProcessor.createParagraph(text, profile)
-				.add(" ")
-				.add(profile.getLink("See more", serverName + ANALYSIS_PATH)));
-		list.add(profile.getParagraph(String.format(Texts.getProperty("identifiers.found"),
-				found, found + notFound, analysisData.getResult().getPathways().size())));
+        if (analysisTypeDescription != null && analysisTypeSeeMoreLink != null) {
+            paragraphs.add(HtmlProcessor.createParagraph(analysisTypeDescription, profile)
+                    .add(" ")
+                    .add(analysisTypeSeeMoreLink));
+        }
 
-		if (analysisData.isProjection())
-			list.add(profile.getParagraph(Texts.getProperty("projected"))
-					.add(" ")
-					.add(profile.getLink("\u2197", serverName + PROJECTED)));
+        paragraphs.add(profile.getParagraph(String.format(Texts.getProperty("identifiers.found"),
+                found, found + notFound, analysisData.getResult().getPathways().size())));
 
-		if (analysisData.isInteractors())
-			list.add(profile.getParagraph(Texts.getProperty("interactors")));
+        if (analysisData.isProjection())
+            paragraphs.add(profile.getParagraph(Texts.getProperty("projected"))
+                    .add(" ")
+                    .add(profile.getLink("\u2197", serverName + PROJECTED)));
 
-		list.add(profile.getParagraph((Texts.getProperty("target.species.resource", analysisData.getSpecies(), analysisData.getBeautifiedResource()))));
+        if (analysisData.isInteractors())
+            paragraphs.add(profile.getParagraph(Texts.getProperty("interactors")));
 
-		list.add(profile.getParagraph(String.format(Texts.getProperty("token"), summary.getToken())));
+        paragraphs.add(profile.getParagraph((Texts.getProperty("target.species.resource", analysisData.getSpecies(), analysisData.getBeautifiedResource()))));
 
-		document.add(profile.getList(list));
-	}
+        paragraphs.add(profile.getParagraph(String.format(Texts.getProperty("token"), summary.getToken())));
+
+        document.add(profile.getList(paragraphs));
+    }
 
 }
