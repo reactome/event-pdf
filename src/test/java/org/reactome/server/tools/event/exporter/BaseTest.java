f5569deb5bb2f4a7acc60593ba359dd2aa7f4a5a
EliotRagueneau <159753.AZErty>
2021-10-26 16:37:50 +0100

    :white_check_mark: Make test almost work in  CommandLine: still issues with Abstract Class within graph-core

--- src/test/java/org/reactome/server/tools/event/exporter/BaseTest.java
+++ src/test/java/org/reactome/server/tools/event/exporter/BaseTest.java
@@ -3,18 +3,22 @@ package org.reactome.server.tools.event.exporter;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.reactome.server.graph.aop.LazyFetchAspect;
 import org.reactome.server.graph.config.GraphCoreNeo4jConfig;
 import org.reactome.server.graph.service.GeneralService;
+import org.reactome.server.graph.utils.ReactomeGraphCore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+@SpringBootTest
 @ContextConfiguration(classes = {GraphCoreNeo4jConfig.class})
 @ExtendWith(SpringExtension.class)
 public abstract class BaseTest {
@@ -32,6 +36,13 @@ public abstract class BaseTest {
 
     @AfterAll
     public static void tearDownClass() {
+    }
+
+    @BeforeAll
+    public static void setUpStatic(@Value("${spring.neo4j.uri}") String uri,
+                                   @Value("${spring.neo4j.authentication.username}") String user,
+                                   @Value("${spring.neo4j.authentication.password}") String pass) {
+        ReactomeGraphCore.initialise(uri, user, pass);
     }
 
     @BeforeEach
